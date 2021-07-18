04efdb3908bb8b3361697d23d7d73da636fb25de
MaryLima4 <maryanelima.87@gmail.com>
2021-07-18 14:15:14 -0300

    Atualizando os arquivos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,28 @@ const form = document.getElementById('form')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
+
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
+    console.log(nome, email);
+
+    let enviar = {
+        nome,
+        email,
+    }
+    let convertEnvio = JSON.stringify(enviar);
+
+    localStorage.setItem('lead', convertEnvio);
+
+    let conteudo = document.getElementById('conteudo');
+
+    let enviando = `<img src="/imagens/loader.gif" alt="Carregando">`
+
+    let enviado = `Formulário enviado com sucesso!`
+
+    conteudo.innerHTML = enviando
+
+    setTimeout(() => {
+        conteudo.innerHTML = enviado
+    }, 2000)
 })
